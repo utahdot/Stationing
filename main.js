7659883fbf0300c2148b23964c50370ad4d4fa06
Alex Gilvarry <alex.gilvarry@gmail.com>
2021-04-14 10:06:05 -0600

    stationing button test

--- main.js
+++ main.js
@@ -1,25 +1,32 @@
 require(["esri/request"], function(esriRequest) {
+    "use strict"
     const resultsDiv = document.getElementById("resultsDiv");
-    const input = document.getElementById("inputUrl");
+    const latInput = document.getElementById("latitude");
+    const lonInput = document.getElementById("longitude");
 
-    // Define the 'options' for the request
-    var geometryToStation = {
-      query: {
-        locations: `[{"routeId" : "", "geometry" : { "x" : ${latitude}, "y" : ${longitude} }}]`,
-        inSR: 26912,
-        outSR: 4326,
-        f: "json"
-      },
-      responseType: "json"
-    };
 
     // Make the request on a button click using the
     // value of the 'input' text
-    btnQuery.addEventListener("click", function() {
-      var url = input.value;
-      esriRequest(url, geometryToStation).then(function(response) {
+    getStation.addEventListener("click", function(btn) {
+        const latitude = latInput.value;
+        const longitude = lonInput.value;
+        const url = btn.target.value;
+        
+        const options = {
+            query: {
+              locations: `[{"routeId" : "", "geometry" : { "x" : ${latitude}, "y" : ${longitude} }}]`,
+              inSR: 26912,
+              outSR: 4326,
+              f: "json"
+            },
+            responseType: "json"
+          };
+
+      
+      
+      esriRequest(url, options).then(function(response) {
         console.log("response", response);
-        var responseJSON = JSON.stringify(response, null, 2);
+        let responseJSON = JSON.stringify(response, null, 2);
         resultsDiv.innerHTML = responseJSON;
       });
     });
