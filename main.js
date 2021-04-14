db2c118efa8f7cd06ba794a4d594ccd6d5121078
Alex Gilvarry <alex.gilvarry@gmail.com>
2021-04-14 10:10:54 -0600

    swap lat/lon for x/y

--- main.js
+++ main.js
@@ -1,28 +1,26 @@
 require(["esri/request"], function(esriRequest) {
     "use strict"
     const resultsDiv = document.getElementById("resultsDiv");
-    const latInput = document.getElementById("latitude");
-    const lonInput = document.getElementById("longitude");
+    const xInput = document.getElementById("X");
+    const yInput = document.getElementById("Y");
 
 
     // Make the request on a button click using the
     // value of the 'input' text
     getStation.addEventListener("click", function(btn) {
-        const latitude = latInput.value;
-        const longitude = lonInput.value;
+        const X = xInput.value;
+        const Y = yInput.value;
         const url = btn.target.value;
         
-        const options = {
+        let options = {
             query: {
-              locations: `[{"routeId" : "", "geometry" : { "x" : ${latitude}, "y" : ${longitude} }}]`,
+              locations: `[{"routeId" : "", "geometry" : { "x" : ${X}, "y" : ${Y} }}]`,
               inSR: 26912,
               outSR: 4326,
               f: "json"
             },
             responseType: "json"
           };
-
-      
       
       esriRequest(url, options).then(function(response) {
         console.log("response", response);
